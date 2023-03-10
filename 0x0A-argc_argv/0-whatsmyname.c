dc11e6fa2344b02d2ab69e7d57b6264538abfa3e
idris <idico92@gmail.com>
2023-03-10 15:38:17 -0800

    name

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -7,10 +7,10 @@
  *
  * Return: 0
  */
-int main(int argc, char **argv)
+int main(int argc, char *argv[])
 {
 	(void)argc;
 
-	printf("%s\n", argv[0]);
-	return 0;
+	printf("%s\n", *argv[0]);
+	return (0);
 }
