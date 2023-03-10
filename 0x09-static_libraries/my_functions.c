01fd131ed3072d74f591aa5e1c00a8998058be99
idris <idico92@gmail.com>
2023-03-10 14:07:29 -0800

    0x09-static_libraries

---
+++ 0x09-static_libraries/my_functions.c
@@ -0,0 +1,101 @@
+#include "main.h"
+
+int _putchar(char c)
+{
+    // implementation of the _putchar function
+}
+
+int _islower(int c)
+{
+    // implementation of the _islower function
+}
+
+int _isalpha(int c)
+{
+    // implementation of the _isalpha function
+}
+
+int _abs(int n)
+{
+    // implementation of the _abs function
+}
+
+int _isupper(int c)
+{
+    // implementation of the _isupper function
+}
+
+int _isdigit(int c)
+{
+    // implementation of the _isdigit function
+}
+
+int _strlen(char *s)
+{
+    // implementation of the _strlen function
+}
+
+void _puts(char *s)
+{
+    // implementation of the _puts function
+}
+
+char *_strcpy(char *dest, char *src)
+{
+    // implementation of the _strcpy function
+}
+
+int _atoi(char *s)
+{
+    // implementation of the _atoi function
+}
+
+char *_strcat(char *dest, char *src)
+{
+    // implementation of the _strcat function
+}
+
+char *_strncat(char *dest, char *src, int n)
+{
+    // implementation of the _strncat function
+}
+
+char *_strncpy(char *dest, char *src, int n)
+{
+    // implementation of the _strncpy function
+}
+
+int _strcmp(char *s1, char *s2)
+{
+    // implementation of the _strcmp function
+}
+
+char *_memset(char *s, char b, unsigned int n)
+{
+    // implementation of the _memset function
+}
+
+char *_memcpy(char *dest, char *src, unsigned int n)
+{
+    // implementation of the _memcpy function
+}
+
+char *_strchr(char *s, char c)
+{
+    // implementation of the _strchr function
+}
+
+unsigned int _strspn(char *s, char *accept)
+{
+    // implementation of the _strspn function
+}
+
+char *_strpbrk(char *s, char *accept)
+{
+    // implementation of the _strpbrk function
+}
+
+char *_strstr(char *haystack, char *needle)
+{
+    // implementation of the _strstr function
+}
